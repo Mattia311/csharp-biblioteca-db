14ec15efb9cc7d3835c8059ec18828d4d9978201
matte <matte@DESKTOP-LP3MD07>
2022-05-26 18:07:40 +0200

    Aggiungere i file di progetto.

---
+++ Autore.cs
@@ -0,0 +1,26 @@
+﻿using ccsharp_biblioteca1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_biblioteca1
+{
+    internal class Autore : Persona
+    {
+        public long CodiceAutore;
+        public string email;
+        public Autore(string Nome, string Cognome, string Email) : base(Nome, Cognome)
+        {
+            this.email = Email;
+            this.CodiceAutore = GeneraCodiceAutore();
+
+        }
+
+        public long GeneraCodiceAutore()
+        {
+            return DB.GetUniqueId();
+        }
+    }
+}
